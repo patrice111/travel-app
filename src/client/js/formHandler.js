ceb4009f8dc389221404c825a0ed2a8a21011cbb
Patrice Codes <42042567+patrice111@users.noreply.github.com>
2021-03-30 07:31:15 -0400

    Add files via upload

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -40,29 +40,6 @@ function handleSubmit(event) {
         }
     })
     .catch(err => console.log('err==>>', err));
-    //check what text was put into the form field
-    //let formText = document.getElementById('url').value
-    if(Client.checkUrl(formText))
-    console.log("::: Form Submitted :::")
-
-    
-    fetch('http://localhost:8081/article', {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-        "Content-Type": "application/json"
-        },
-        body: JSON.stringify({url: formText}),
-    })
-    .then(res => {
-        return res.json()
-    })
-    .then(function(res) {
-        document.getElementById("positive").innerHTML = `Positive: ${res.positive}`;
-        document.getElementById("neutral").innerHTML = `Neutral: ${res.neutral}`;
-        document.getElementById("negative").innerHTML = `Negative: ${res.negative}`;
-        document.getElementById("without sentiment").innerHTML = `Without Sentiment: ${res.without_sentiment}`;
-    })
 }
 
     
